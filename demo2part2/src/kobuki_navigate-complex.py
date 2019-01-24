7ce61a00b10c55a5e811ebcd23b5e07355c559e3
Brad Ofrim <bofrim@gmail.com>
2019-01-23 21:49:27 -0700

    black

--- demo2part2/src/kobuki_navigate-complex.py
+++ demo2part2/src/kobuki_navigate-complex.py
@@ -7,13 +7,16 @@ import threading
 from geometry_msgs.msg import Twist
 from kobuki_msgs.msg import BumperEvent
 
+
 class Forward(smach.State):
     def __init__(self, movement_pub_node):
-        smach.State.__init__(self, outcomes=['backup', 'finished'])
+        smach.State.__init__(self, outcomes=["backup", "finished"])
         self.mutex = threading.Lock()
 
         self.bumper_hit = False
-        self.bumper_node = rospy.Subscriber('/mobile_base/events/bumper', BumperEvent, self.bumper_callback)
+        self.bumper_node = rospy.Subscriber(
+            "/mobile_base/events/bumper", BumperEvent, self.bumper_callback
+        )
         self.kobuki_movement = movement_pub_node
 
     def bumper_callback(self, msg):
@@ -23,12 +26,12 @@ class Forward(smach.State):
         self.mutex.release()
 
     def __execute__(self):
-        while(True):
+        while True:
             self.mutex.acquire()
             if self.bumper_hit:
-                #TODO: Implement different turning options
-                return 'turn_clockwise'
-            
+                # TODO: Implement different turning options
+                return "turn_clockwise"
+
             self.mutex.release()
 
             twist = Twist()
@@ -38,16 +41,16 @@ class Forward(smach.State):
 
             self.kobuki_movement.publish(twist)
 
-        
+
 class TurnToHorizontal(smach.State):
     def __init__(self, movement_pub_node):
-        smach.State.__init__(self, outcomes=['forward', 'left'])
+        smach.State.__init__(self, outcomes=["forward", "left"])
         self.kobuki_movement = movement_pub_node
 
     def __execute__(self):
 
         # Change this to use encoders to know when to break
-        while(True):
+        while True:
             twist = Twist()
             twist.angular.z = 0.1
 
@@ -59,18 +62,18 @@ class TurnToHorizontal(smach.State):
         #     - and have a state for turning right -> straight
 
         # TODO: Change this
-        return 'forward'
+        return "forward"
 
 
 class TurnToVertical(smach.State):
     def __init__(self, movement_pub_node):
-        smach.State.__init__(self, outcomes=['forward', 'right'])
+        smach.State.__init__(self, outcomes=["forward", "right"])
         self.kobuki_movement = movement_pub_node
 
     def __execute__(self):
 
         # Change this to use encoders to know when to break
-        while(True):
+        while True:
             twist = Twist()
             twist.angular.z = -0.1
 
@@ -82,29 +85,30 @@ class TurnToVertical(smach.State):
         #     - and have a state for turning right -> straight
 
         # TODO: Change this
-        return 'forward'
-
-
+        return "forward"
 
 
 def main():
-    rospy.init_node('nav_state_machine')
+    rospy.init_node("nav_state_machine")
 
-    state_machine = smach.StateMachine(outcomes=['exit'])
-    state_introspection_server = smach_ros.IntrospectionServer('server_name', state_machine, '/SM_ROOT')
+    state_machine = smach.StateMachine(outcomes=["exit"])
+    state_introspection_server = smach_ros.IntrospectionServer(
+        "server_name", state_machine, "/SM_ROOT"
+    )
     state_introspection_server.start()
 
-    cmd_vel_pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
+    cmd_vel_pub = rospy.Publisher("cmd_vel", Twist, queue_size=1)
 
     with state_machine:
-        smach.StateMachine.add('FORWARD', Forward(cmd_vel_pub),
-                                transitions={'backup': 'BACKUP',
-                                             'finished': 'FINISHED'})
-
+        smach.StateMachine.add(
+            "FORWARD",
+            Forward(cmd_vel_pub),
+            transitions={"backup": "BACKUP", "finished": "FINISHED"},
+        )
 
     state_machine.execute()
     state_introspection_server.stop()
-                                    
-if __name__ == '__main__':
-    main()
 
+
+if __name__ == "__main__":
+    main()
--- demo2part2/src/kobuki_navigate.py
+++ demo2part2/src/kobuki_navigate.py
@@ -34,19 +34,21 @@ def odom_callback(msg):
 
     pose = numpify(msg.pose.pose)
     _, _, angles, _, _ = decompose_matrix(pose)
-    
+
     g_x = msg.pose.pose.position.x
     g_y = msg.pose.pose.position.y
     g_z = msg.pose.pose.position.z
     g_theta = angles[2] * 180 / 3.14159
 
+
 def bumper_callback(msg):
     global g_bumper
     g_bumper = bool(msg.state)
 
+
 class Forward(smach.State):
     def __init__(self, movement_pub_node):
-        smach.State.__init__(self, outcomes=['backup', 'finished'])
+        smach.State.__init__(self, outcomes=["backup", "finished"])
         self.rate = rospy.Rate(TWIST_PUB_FREQ)
         self.kobuki_movement = movement_pub_node
 
@@ -58,22 +60,22 @@ class Forward(smach.State):
             self.kobuki_movement.publish(twist)
 
             if g_x > FINISH_DISTANCE_M:
-                return 'finished'
-            
+                return "finished"
+
             self.rate.sleep()
 
         self.kobuki_movement.publish(Twist())
-        return 'backup'            
+        return "backup"
 
 
 class Backup(smach.State):
     def __init__(self, movement_pub_node):
-        smach.State.__init__(self, outcomes=['turn_horizontal'])
+        smach.State.__init__(self, outcomes=["turn_horizontal"])
         self.rate = rospy.Rate(TWIST_PUB_FREQ)
         self.kobuki_movement = movement_pub_node
 
     def execute(self, userdata):
-        #TODO: backup for a set amount of distance
+        # TODO: backup for a set amount of distance
         backup_twist = Twist()
         backup_twist.linear.x = -0.2
         num_backup_msgs = 10
@@ -83,12 +85,12 @@ class Backup(smach.State):
             self.rate.sleep()
 
         self.kobuki_movement.publish(Twist())
-        return 'turn_horizontal'
+        return "turn_horizontal"
 
-    
+
 class TurnHorizontal(smach.State):
     def __init__(self, movement_pub_node):
-        smach.State.__init__(self, outcomes=['move_horizontal'])
+        smach.State.__init__(self, outcomes=["move_horizontal"])
         self.kobuki_movement = movement_pub_node
 
     def execute(self, userdata):
@@ -101,12 +103,12 @@ class TurnHorizontal(smach.State):
         elif 80 < g_theta and g_theta < 100:
             turn_kobuki(-90, self.kobuki_movement)
 
-        return 'move_horizontal'
+        return "move_horizontal"
 
 
 class Horizontal(smach.State):
     def __init__(self, movement_pub_node):
-        smach.State.__init__(self, outcomes=['turn_forward', 'backup'])
+        smach.State.__init__(self, outcomes=["turn_forward", "backup"])
         self.rate = rospy.Rate(TWIST_PUB_FREQ)
         self.kobuki_movement = movement_pub_node
 
@@ -117,95 +119,107 @@ class Horizontal(smach.State):
         while abs(g_y - initial_y) < HORIZONTAL_THRESHOLD:
             if g_bumper:
                 self.kobuki_movement.publish(Twist())
-                return 'backup'
-            
+                return "backup"
+
             forward_twist = Twist()
             forward_twist.linear.x = 0.2
             self.kobuki_movement.publish(forward_twist)
             self.rate.sleep()
 
-
         self.kobuki_movement.publish(Twist())
-        return 'turn_forward'
+        return "turn_forward"
+
 
 class TurnForward(smach.State):
     def __init__(self, movement_pub_node):
-        smach.State.__init__(self, outcomes=['move_forward'])
+        smach.State.__init__(self, outcomes=["move_forward"])
         self.kobuki_movement = movement_pub_node
 
     def execute(self, userdata):
         global g_theta
         turn_kobuki(0, self.kobuki_movement)
 
-        return 'move_forward'
+        return "move_forward"
+
 
 class Finished(smach.State):
     def __init__(self):
-        smach.State.__init__(self, outcomes=['exit'])
-        
+        smach.State.__init__(self, outcomes=["exit"])
+
     def execute(self, userdata):
         # TODO: Play a nice song / flash light
-        return 'exit'
+        return "exit"
 
 
-def turn_kobuki(desired_angle, kobuki_pub_node, angle_tolerance = 2):
+def turn_kobuki(desired_angle, kobuki_pub_node, angle_tolerance=2):
     global g_theta
     rate = rospy.Rate(TWIST_PUB_FREQ)
     lower_bound = desired_angle - angle_tolerance
     upper_bound = desired_angle + angle_tolerance
-    
- 
-    while( g_theta < lower_bound or g_theta > upper_bound):
-        rotation_direction = (desired_angle - g_theta) / abs( desired_angle - g_theta)
+
+    while g_theta < lower_bound or g_theta > upper_bound:
+        rotation_direction = (desired_angle - g_theta) / abs(desired_angle - g_theta)
 
         twist = Twist()
         twist.angular.z = rotation_direction * ANGULAR_VELOCITY
 
         kobuki_pub_node.publish(twist)
         rate.sleep()
-             
+
 
 def main():
-    rospy.init_node('nav_state_machine')
+    rospy.init_node("nav_state_machine")
 
-    state_machine = smach.StateMachine(outcomes=['exit'])
-    state_introspection_server = smach_ros.IntrospectionServer('server_name', state_machine, '/SM_ROOT')
+    state_machine = smach.StateMachine(outcomes=["exit"])
+    state_introspection_server = smach_ros.IntrospectionServer(
+        "server_name", state_machine, "/SM_ROOT"
+    )
     state_introspection_server.start()
 
-    cmd_vel_pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
-    
-    odom_sub = rospy.Subscriber('/odom', Odometry, odom_callback)
-    bumper_sub = rospy.Subscriber('/mobile_base/events/bumper', BumperEvent, bumper_callback)
+    cmd_vel_pub = rospy.Publisher("cmd_vel", Twist, queue_size=1)
 
+    odom_sub = rospy.Subscriber("/odom", Odometry, odom_callback)
+    bumper_sub = rospy.Subscriber(
+        "/mobile_base/events/bumper", BumperEvent, bumper_callback
+    )
 
     with state_machine:
-        smach.StateMachine.add("FORWARD", Forward(cmd_vel_pub),
-                                transitions={"backup": "BACKUP",
-                                             "finished": "FINISHED"})
-        
-        smach.StateMachine.add("BACKUP", Backup(cmd_vel_pub),
-                                transitions={"turn_horizontal": "TURN_HORIZONTAL"})
+        smach.StateMachine.add(
+            "FORWARD",
+            Forward(cmd_vel_pub),
+            transitions={"backup": "BACKUP", "finished": "FINISHED"},
+        )
 
-        smach.StateMachine.add("TURN_HORIZONTAL", TurnHorizontal(cmd_vel_pub),
-                                transitions={"move_horizontal": "MOVE_HORIZONTAL"})
+        smach.StateMachine.add(
+            "BACKUP",
+            Backup(cmd_vel_pub),
+            transitions={"turn_horizontal": "TURN_HORIZONTAL"},
+        )
 
-        smach.StateMachine.add("MOVE_HORIZONTAL", Horizontal(cmd_vel_pub),
-                                transitions={"backup": "BACKUP",
-                                             "turn_forward": "TURN_FORWARD"})
+        smach.StateMachine.add(
+            "TURN_HORIZONTAL",
+            TurnHorizontal(cmd_vel_pub),
+            transitions={"move_horizontal": "MOVE_HORIZONTAL"},
+        )
 
-        smach.StateMachine.add("TURN_FORWARD", TurnForward(cmd_vel_pub),
-                                transitions={"move_forward": "FORWARD"})
+        smach.StateMachine.add(
+            "MOVE_HORIZONTAL",
+            Horizontal(cmd_vel_pub),
+            transitions={"backup": "BACKUP", "turn_forward": "TURN_FORWARD"},
+        )
 
-        smach.StateMachine.add("FINISHED", Finished(),
-                                transitions={"exit": "exit"})
+        smach.StateMachine.add(
+            "TURN_FORWARD",
+            TurnForward(cmd_vel_pub),
+            transitions={"move_forward": "FORWARD"},
+        )
+
+        smach.StateMachine.add("FINISHED", Finished(), transitions={"exit": "exit"})
 
     state_machine.execute()
 
     state_introspection_server.stop()
 
-                                    
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
-
-
-

8e8937bc022461cfbcee4ee7d7b1ae1785c66800
Brad Ofrim <bofrim@gmail.com>
2019-01-30 21:29:37 -0700

    black

--- demo3/src/RobberSM.py
+++ demo3/src/RobberSM.py
@@ -24,6 +24,7 @@ OUTER_DISTANCE_LIMIT = 1.0
 global g_zone_distances
 global g_bumped
 
+
 def zone_callback(msg):
     global g_zone_distances
     g_zone_distances = msg.distances
@@ -35,31 +36,42 @@ def bumper_callback(msg):
 
 
 def is_right_clear(zones):
-    return (zones[OUTER_RIGHT] >= OUTER_DISTANCE_LIMIT and
-            zones[RIGHT] >= NORMAL_DISTANCE_LIMIT and 
-            zones[INNER_RIGHT] >= INNER_DISTANCE_LIMIT)
+    return (
+        zones[OUTER_RIGHT] >= OUTER_DISTANCE_LIMIT
+        and zones[RIGHT] >= NORMAL_DISTANCE_LIMIT
+        and zones[INNER_RIGHT] >= INNER_DISTANCE_LIMIT
+    )
 
 
 def is_left_clear(zones):
-    return (zones[OUTER_LEFT] >= OUTER_DISTANCE_LIMIT and
-            zones[LEFT] >= NORMAL_DISTANCE_LIMIT and 
-            zones[INNER_LEFT] >= INNER_DISTANCE_LIMIT)
+    return (
+        zones[OUTER_LEFT] >= OUTER_DISTANCE_LIMIT
+        and zones[LEFT] >= NORMAL_DISTANCE_LIMIT
+        and zones[INNER_LEFT] >= INNER_DISTANCE_LIMIT
+    )
 
 
 def is_straight_ahead_blocked(zones):
-    return (zones[INNER_LEFT] < INNER_DISTANCE_LIMIT and
-            zones[INNER_RIGHT] < INNER_DISTANCE_LIMIT)
+    return (
+        zones[INNER_LEFT] < INNER_DISTANCE_LIMIT
+        and zones[INNER_RIGHT] < INNER_DISTANCE_LIMIT
+    )
 
 
 def is_straight_ahead_clear(zones):
-        return (zones[INNER_LEFT] >= INNER_DISTANCE_LIMIT and
-            zones[LEFT] >= NORMAL_DISTANCE_LIMIT and
-            zones[INNER_RIGHT] >= INNER_DISTANCE_LIMIT and
-            zones[RIGHT] >= NORMAL_DISTANCE_LIMIT)
+    return (
+        zones[INNER_LEFT] >= INNER_DISTANCE_LIMIT
+        and zones[LEFT] >= NORMAL_DISTANCE_LIMIT
+        and zones[INNER_RIGHT] >= INNER_DISTANCE_LIMIT
+        and zones[RIGHT] >= NORMAL_DISTANCE_LIMIT
+    )
+
 
 def is_straight_ahead_clear_nimble(zones):
-        return (zones[INNER_LEFT] >= INNER_DISTANCE_LIMIT and
-            zones[INNER_RIGHT] >= INNER_DISTANCE_LIMIT)
+    return (
+        zones[INNER_LEFT] >= INNER_DISTANCE_LIMIT
+        and zones[INNER_RIGHT] >= INNER_DISTANCE_LIMIT
+    )
 
 
 def calculate_veer_angular_velocity(inner_distance, normal_distance, outer_distance):
@@ -77,10 +89,12 @@ def calculate_veer_angular_velocity(inner_distance, normal_distance, outer_dista
 
 class Straight(smach.State):
     def __init__(self, rate, pub_node):
-        smach.State.__init__(self, outcomes=['veer_left','veer_right','stuck','bump','exit'])
+        smach.State.__init__(
+            self, outcomes=["veer_left", "veer_right", "stuck", "bump", "exit"]
+        )
         self.vel_pub = pub_node
         self.rate = rate
-    
+
     def execute(self, userdata):
         global g_zone_distances
         global g_bumped
@@ -88,29 +102,29 @@ class Straight(smach.State):
             current_zones = g_zone_distances
 
             if g_bumped:
-                return 'bump'
+                return "bump"
 
             elif is_straight_ahead_blocked(current_zones):
                 self.vel_pub.publish(Twist())
-                return 'stuck'
+                return "stuck"
 
             elif not is_left_clear(current_zones):
-                return 'veer_right'
+                return "veer_right"
 
             elif not is_right_clear(current_zones):
-                return 'veer_left'
+                return "veer_left"
 
             twist = Twist()
             twist.linear.x = 0.8
             self.vel_pub.publish(twist)
             self.rate.sleep()
 
-        return 'exit'
+        return "exit"
 
 
 class VeerLeft(smach.State):
     def __init__(self, rate, pub_node):
-        smach.State.__init__(self, outcomes=['straight','stuck','bump','exit'])
+        smach.State.__init__(self, outcomes=["straight", "stuck", "bump", "exit"])
         self.vel_pub = pub_node
         self.rate = rate
         self.direction = 1
@@ -122,27 +136,34 @@ class VeerLeft(smach.State):
             current_zones = g_zone_distances
 
             if g_bumped:
-                return 'bump'
+                return "bump"
 
             elif is_right_clear(current_zones):
-                return 'straight'
+                return "straight"
 
             elif not is_left_clear(current_zones):
                 self.vel_pub.publish(Twist())
-                return 'stuck'
+                return "stuck"
 
             twist = Twist()
-            twist.angular.z = calculate_veer_angular_velocity(current_zones[INNER_RIGHT], current_zones[RIGHT], current_zones[OUTER_RIGHT]) * self.direction
+            twist.angular.z = (
+                calculate_veer_angular_velocity(
+                    current_zones[INNER_RIGHT],
+                    current_zones[RIGHT],
+                    current_zones[OUTER_RIGHT],
+                )
+                * self.direction
+            )
             twist.linear.x = 0.5 - abs(twist.angular.z)
             self.vel_pub.publish(twist)
             self.rate.sleep()
 
-        return 'exit'
-            
+        return "exit"
+
 
 class VeerRight(smach.State):
     def __init__(self, rate, pub_node):
-        smach.State.__init__(self, outcomes=['straight','stuck','bump','exit'])
+        smach.State.__init__(self, outcomes=["straight", "stuck", "bump", "exit"])
         self.vel_pub = pub_node
         self.rate = rate
         self.direction = -1
@@ -154,34 +175,41 @@ class VeerRight(smach.State):
             current_zones = g_zone_distances
 
             if g_bumped:
-                return 'bump'
+                return "bump"
 
             elif is_left_clear(current_zones):
-                return 'straight'
+                return "straight"
 
             elif not is_right_clear(current_zones):
                 self.vel_pub.publish(Twist())
-                return 'stuck'
+                return "stuck"
 
             twist = Twist()
-            twist.angular.z = calculate_veer_angular_velocity(current_zones[INNER_LEFT], current_zones[LEFT], current_zones[OUTER_LEFT]) * self.direction
+            twist.angular.z = (
+                calculate_veer_angular_velocity(
+                    current_zones[INNER_LEFT],
+                    current_zones[LEFT],
+                    current_zones[OUTER_LEFT],
+                )
+                * self.direction
+            )
             twist.linear.x = 0.5 - abs(twist.angular.z)
             self.vel_pub.publish(twist)
             self.rate.sleep()
 
-        return 'exit'
+        return "exit"
 
 
 class Bumped(smach.State):
     def __init__(self, rate, pub_node):
-        smach.State.__init__(self, outcomes=['stuck', 'exit'])
+        smach.State.__init__(self, outcomes=["stuck", "exit"])
         self.vel_pub = pub_node
         self.rate = rate
 
     def execute(self, userdata):
         self.vel_pub.publish(Twist())
         if rospy.is_shutdown():
-            return 'exit'
+            return "exit"
 
         num_backup_msgs = 5
         backup_twist = Twist()
@@ -191,12 +219,12 @@ class Bumped(smach.State):
             self.vel_pub.publish(backup_twist)
             self.rate.sleep()
 
-        return 'stuck'
+        return "stuck"
 
 
 class Stuck(smach.State):
     def __init__(self, rate, pub_node):
-        smach.State.__init__(self, outcomes=['straight', 'exit'])
+        smach.State.__init__(self, outcomes=["straight", "exit"])
         self.vel_pub = pub_node
         self.rate = rate
 
@@ -206,8 +234,10 @@ class Stuck(smach.State):
 
         self.vel_pub.publish(Twist())
 
-        direction = (g_zone_distances[INNER_LEFT] - g_zone_distances[INNER_RIGHT]) / abs(g_zone_distances[INNER_LEFT] - g_zone_distances[INNER_RIGHT])
-        
+        direction = (
+            g_zone_distances[INNER_LEFT] - g_zone_distances[INNER_RIGHT]
+        ) / abs(g_zone_distances[INNER_LEFT] - g_zone_distances[INNER_RIGHT])
+
         starting_time = time()
         time_limit = 1
         while not rospy.is_shutdown():
@@ -215,18 +245,19 @@ class Stuck(smach.State):
 
             if time() - starting_time > time_limit:
                 if is_straight_ahead_clear_nimble(current_zones):
-                    return 'straight'
+                    return "straight"
 
-            else: 
+            else:
                 if is_straight_ahead_clear(current_zones):
-                    return 'straight'
+                    return "straight"
 
             twist = Twist()
-            twist.angular.z = 0.6 * direction
+            twist.angular.z = 1.0 * direction
             self.vel_pub.publish(twist)
             self.rate.sleep()
 
-        return 'exit'
+        return "exit"
+
 
 def main():
     global g_zone_distances
@@ -235,47 +266,69 @@ def main():
     g_zone_distances = [3.0, 3.0, 3.0, 3.0, 3.0, 3.0]
     g_bumped = False
 
-    rospy.init_node('robber_state_machine')
+    rospy.init_node("robber_state_machine")
 
-    state_machine = smach.StateMachine(outcomes=['exit'])
-    state_introspection_server = smach_ros.IntrospectionServer('server_name', state_machine, '/SM_ROOT')
+    state_machine = smach.StateMachine(outcomes=["exit"])
+    state_introspection_server = smach_ros.IntrospectionServer(
+        "server_name", state_machine, "/SM_ROOT"
+    )
     state_introspection_server.start()
 
-    rospy.Subscriber('zone_scan', ZoneScan, zone_callback)
-    cmd_vel_pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
+    rospy.Subscriber("zone_scan", ZoneScan, zone_callback)
+    cmd_vel_pub = rospy.Publisher("cmd_vel", Twist, queue_size=1)
 
     rate = rospy.Rate(10)
 
     with state_machine:
-        smach.StateMachine.add('STRAIGHT', Straight(rate, cmd_vel_pub),
-                                transitions={'veer_left': 'VEER_LEFT',
-                                            'veer_right': 'VEER_RIGHT',
-                                            'stuck': 'STUCK',
-                                            'bump': 'BUMP',
-                                            'exit': 'exit'})
+        smach.StateMachine.add(
+            "STRAIGHT",
+            Straight(rate, cmd_vel_pub),
+            transitions={
+                "veer_left": "VEER_LEFT",
+                "veer_right": "VEER_RIGHT",
+                "stuck": "STUCK",
+                "bump": "BUMP",
+                "exit": "exit",
+            },
+        )
 
-        smach.StateMachine.add('VEER_LEFT', VeerLeft(rate, cmd_vel_pub),
-                                transitions={'straight': 'STRAIGHT',
-                                            'stuck': 'STUCK',
-                                            'bump': 'BUMP',
-                                            'exit': 'exit'})
+        smach.StateMachine.add(
+            "VEER_LEFT",
+            VeerLeft(rate, cmd_vel_pub),
+            transitions={
+                "straight": "STRAIGHT",
+                "stuck": "STUCK",
+                "bump": "BUMP",
+                "exit": "exit",
+            },
+        )
 
-        smach.StateMachine.add('VEER_RIGHT', VeerRight(rate, cmd_vel_pub),
-                                transitions={'straight': 'STRAIGHT',
-                                            'stuck': 'STUCK',
-                                            'bump': 'BUMP',
-                                            'exit': 'exit'})
+        smach.StateMachine.add(
+            "VEER_RIGHT",
+            VeerRight(rate, cmd_vel_pub),
+            transitions={
+                "straight": "STRAIGHT",
+                "stuck": "STUCK",
+                "bump": "BUMP",
+                "exit": "exit",
+            },
+        )
 
-        smach.StateMachine.add('STUCK', Stuck(rate, cmd_vel_pub),
-                                transitions={'straight': 'STRAIGHT',
-                                            'exit': 'exit'})
+        smach.StateMachine.add(
+            "STUCK",
+            Stuck(rate, cmd_vel_pub),
+            transitions={"straight": "STRAIGHT", "exit": "exit"},
+        )
 
-        smach.StateMachine.add('BUMP', Bumped(rate, cmd_vel_pub),
-                                transitions={'stuck': 'STUCK',
-                                            'exit': 'exit'})
+        smach.StateMachine.add(
+            "BUMP",
+            Bumped(rate, cmd_vel_pub),
+            transitions={"stuck": "STUCK", "exit": "exit"},
+        )
 
     state_machine.execute()
     state_introspection_server.stop()
-                                    
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     main()

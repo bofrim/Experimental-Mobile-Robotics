c0aa59621ca2b0bf62fd68c100d05459888537cc
Jordan Lane <jlane@ualberta.ca>
2019-02-13 21:15:59 -0700

    White Line Centroid Node

---
+++ comp2/src/white_line_tracker.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+import rospy, cv2, cv_bridge, numpy
+import smach, smach_ros
+
+from comp2.msg import Centroid
+from sensor_msgs.msg import Image
+
+class WhiteLineTracker:
+    def __init__(self):
+        self.bridge = cv_bridge.CvBridge()
+        self.image_sub = rospy.Subscriber(
+            "camera/rgb/image_raw", Image, self.image_callback
+        )
+        self.centroid_pub = rospy.Publisher(
+            "white_line_centroid", Centroid, queue_size="1"
+        )
+
+    def image_callback(self, msg):
+        image = self.bridge.imgmsg_to_cv2(msg, desired_encoding="bgr8")
+        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        lower_white = numpy.array([0, 0, 170])
+        upper_white = numpy.array([255, 10, 255])
+        mask = cv2.inRange(hsv, lower_white, upper_white)
+
+        h, w, d = image.shape
+        search_top = h * 0.75
+        search_bot = search_top + 60
+        mask[0:search_top, 0:w] = 0
+        mask[search_bot:h, 0:w] = 0
+        M = cv2.moments(mask)
+        if M["m00"] > 0:
+            cx = int(M["m10"] / M["m00"])
+            cy = int(M["m01"] / M["m00"])
+            cv2.circle(image, (cx, cy), 20, (0, 0, 255), -1)
+
+            centroid_msg = Centroid()
+            centroid_msg.cx = cx
+            centroid_msg.cy = cy
+            self.centroid_pub.publish(centroid_msg)
+
+        cv2.imshow("window", mask)
+        cv2.waitKey(3)
+
+rospy.init_node("white_line_finder")
+follower = WhiteLineTracker()
+rospy.spin()    

9cdbd83c934eff48950ab86111c43b41abef70b6
Brad Ofrim <bofrim@gmail.com>
2019-02-14 07:46:43 -0700

    try to recognize a green shape

--- comp2/src/location3.py
+++ comp2/src/location3.py
@@ -4,6 +4,12 @@ import smach, smach_ros
 
 from operations import simple_turn
 from geometry_msgs.msg import Twist
+from sensor_msgs.msg import Image
+from kobuki_msgs.msg import Led
+
+from location2 import the_shape
+from image_processing import get_red_mask, detect_shape, Shapes
+
 
 class TurnLeft3(smach.State):
     def __init__(self, rate, pub_node):
@@ -17,9 +23,33 @@ class TurnLeft3(smach.State):
 
 
 class Detect3(smach.State):
-    def __init__(self, rate):
+    def __init__(self, rate, light_pubs):
         smach.State.__init__(self, outcomes=["turn_right", "exit"])
         self.rate = rate
 
     def execute(self, userdata):
+        global the_shape
+        red_mask = get_red_mask()
+        shape_moments = zip(detect_shape(red_mask, threshold=500))
+
+        largest_shape = Shapes.unknown
+        largest_mass = 0
+
+        for shape, moment in shape_moments:
+            if moment["m00"] > largest_mass:
+                largest_mass = moment["m00"]
+                largest_shape = shape
+
+        if largest_shape == the_shape:
+            """Found the correct shape."""
+            led_msg = Led()
+            led_msg.value = Led.GREEN
+            self.light_pubs[0].publish(led_msg)
+            self.light_pubs[1].publish(led_msg)
+        else:
+            led_msg = Led()
+            led_msg.value = Led.RED
+            self.light_pubs[0].publish(led_msg)
+            self.light_pubs[1].publish(led_msg)
+
         return "turn_right"

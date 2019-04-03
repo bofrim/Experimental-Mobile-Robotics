8864553f0f0b6ca916e14d3adc8dfd6443c077ca
Jordan-Lane <jlane@ualberta.ca>
2019-04-02 21:21:26 -0600

    Spot by spot scanning (inaccurate waypoints)

--- comp4/src/location4.py
+++ comp4/src/location4.py
@@ -17,15 +17,20 @@ from geometry_msgs.msg import (
     PoseWithCovariance,
     Pose,
 )
+
+from image_processing import study_shapes, get_red_mask
+
 from sensor_msgs.msg import Joy
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 from kobuki_msgs.msg import Led, Sound
 from utils import display_count
 from config_globals import *
 
+from location2 import get_the_shape
+
 class DriverRamp(Drive):
     def __init__(self, rate, pub_node):
-        super(DriverRamp, self).__init__(rate, pub_node, ["drive_to_start", "exit"])
+        super(DriverRamp, self).__init__(rate, pub_node, ["start", "exit"])
 
     def execute(self, userdata):
         self.stop_distance = -1
@@ -51,7 +56,7 @@ class DriverRamp(Drive):
 
                 pose_pub.unregister()
                 white_line_sub.unregister()
-                return "drive_to_start"
+                return "start"
 
             # TODO: Add in condition that robot takes the wrong path and sees a red line
 
@@ -174,193 +179,36 @@ class PushLeft(smach.State):
         pass
 
 
-class DriveToStart(smach.State):
-    def __init__(self, rate, light_pubs):
-        smach.State.__init__(self, outcomes=["ar_survey", "parking_spot", "on_ramp"])
+class ShapeScan(smach.State):
+    def __init__(self, rate, pub_node, led_nodes):
+        smach.State.__init__(self, outcomes=["on_ramp", "exit"])
+        self.rate = rate
+        self.pub_node = pub_node
+        self.led_nodes = led_nodes
         self.client = actionlib.SimpleActionClient("move_base", MoveBaseAction)
         self.client.wait_for_server()
-        self.start_pose = MoveBaseGoal()
-        self.start_pose.target_pose.header.frame_id = "map"
-        self.start_pose.target_pose.pose = WAYPOINT_MAP["scan"]
-        self.current_task = 0
-        self.task_list = ["ar_survey", "parking_spot", "on_ramp"]
-        self.light_pubs = light_pubs
 
     def execute(self, userdata):
-        if self.current_task == 0:
-            init_pose = MoveBaseGoal()
-            init_pose.target_pose.header.frame_id = "map"
-            init_pose.target_pose.pose = WAYPOINT_MAP["8"]
-            rospy.sleep(0.5)
+        for spot_num in range(8, 0, -1):
+            parking_spot = MoveBaseGoal()
+            parking_spot.target_pose.header.frame_id = "map"
+            parking_spot.target_pose.pose = WAYPOINT_MAP[str(spot_num)]
 
-            self.client.send_goal(init_pose)
+            self.client.send_goal(parking_spot)
             self.client.wait_for_result()
 
-        rospy.sleep(0.5)
-        self.client.send_goal(self.start_pose)
-        self.client.wait_for_result()
+            shape = study_shapes(
+                get_red_mask, max_samples=60, confidence=0.5
+            )        
 
-        next_state = self.task_list[self.current_task]
-        self.current_task += 1
-        display_count(0, self.light_pubs)
-        return next_state
+            '''
+            if shape == get_the_shape():
+                display_count(2, self.led_nodes, color_primary=Led.RED)
+                break 
+            '''
 
-
-class ArSurvey(smach.State):
-    def __init__(self, rate, pub_node):
-        smach.State.__init__(
-            self, outcomes=["ar_approach", "exit"], output_keys=["target_marker"]
-        )
-        self.pub_node = pub_node
-        self.target_marker = None
-        self.ar_focus_id = -1
-        self.target_repetitions = 0
-
-    def execute(self, userdata):
-        self.target_marker = None
-        rate = rospy.Rate(10)
-        ar_sub = rospy.Subscriber(
-            MARKER_POSE_TOPIC, AlvarMarkers, self.ar_callback, queue_size=1
-        )
-
-        while not rospy.is_shutdown():
-            if (
-                self.target_marker
-                and self.target_repetitions >= 3
-                and -0.3 < self.target_marker.pose.pose.position.y
-                and self.target_marker.pose.pose.position.y < 0.3
-            ):
-                userdata.target_marker = self.target_marker
-                ar_sub.unregister()
-                return "ar_approach"
-
-            twist_msg = Twist()
-            twist_msg.angular.z = 0.3
-
-            self.pub_node.publish(twist_msg)
-            rate.sleep()
-
-        ar_sub.unregister()
-        return "exit"
-
-    def ar_callback(self, msg):
-        if not msg.markers:
-            self.ar_focus_id = -1
-            self.target_repetitions = 0
-            return
-
-        for marker in msg.markers:
-            if marker.id == self.ar_focus_id:
-                self.target_repetitions = self.target_repetitions + 1
-            else:
-                self.ar_focus_id = marker.id
-                self.target_repetitions = 0
-
-            self.target_marker = marker
-            break
-
-
-class ArApproach(smach.State):
-    def __init__(self, rate, pub_node, sound_pub, light_pubs):
-        smach.State.__init__(
-            self, outcomes=["drive_to_start", "exit"], input_keys=["target_marker"]
-        )
-        self.pub_node = pub_node
-        self.target_marker = None
-        self.target_marker_id = None
-        self.target_marker_frame = None
-        self.sound_pub = sound_pub
-        self.light_pubs = light_pubs
-        self.rate = rate
-        self.client = actionlib.SimpleActionClient("move_base", MoveBaseAction)
-        self.client.wait_for_server()
-
-    def execute(self, userdata):
-        self.target_marker = userdata.target_marker
-        self.target_marker_id = userdata.target_marker.id
-        self.target_marker_frame = "ar_marker_" + str(self.target_marker_id)
-
-        ar_sub = rospy.Subscriber(
-            MARKER_POSE_TOPIC, AlvarMarkers, self.ar_callback, queue_size=1
-        )
-
-        print("Approach with cmd_vel")
-        while (
-            self.target_marker.pose.pose.position.x > 0.80 and not rospy.is_shutdown()
-        ):
-            direction = self.target_marker.pose.pose.position.y / abs(
-                self.target_marker.pose.pose.position.y
-            )
-            msg = Twist()
-            msg.angular.z = 0.2 * direction
-            msg.linear.x = 0.2
-            self.pub_node.publish(msg)
-            print("Distance = ", self.target_marker.pose.pose.position.x)
-
-        if rospy.is_shutdown():
-            ar_sub.unregister()
-            return "exit"
-
-        print("Approach with planner")
-        self.client.send_goal(self.calculate_target())
-        self.client.wait_for_result()
-
-        sound_msg = Sound()
-        sound_msg.value = Sound.ON
-        self.sound_pub.publish(sound_msg)
-        display_count(3, self.light_pubs, color_primary=Led.GREEN)
-        for _ in range(8):
-            self.rate.sleep()
-
-        rospy.sleep(2)
-        ar_sub.unregister()
-        return "drive_to_start"
-
-    def calculate_target(self):
-        goal = MoveBaseGoal()
-        goal.target_pose.header.frame_id = self.target_marker_frame
-        goal.target_pose.pose.position = Point(0.0, 0.0, 0.25)
-        goal.target_pose.pose.orientation = Quaternion(0, 0, 0.84147098, 0.54030231)
-        return goal
-
-    def ar_callback(self, msg):
-        for marker in msg.markers:
-            if marker.id == self.target_marker_id:
-                self.target_marker = marker
-
-
-class ParkingSpot(smach.State):
-    def __init__(self, rate, parking_spot, sound_pub, light_pubs):
-        smach.State.__init__(self, outcomes=["drive_to_start"])
-        self.client = actionlib.SimpleActionClient("move_base", MoveBaseAction)
-        self.client.wait_for_server()
-        self.parking_spot = parking_spot
-        self.rate = rate
-        self.sound_pub = sound_pub
-        self.light_pubs = light_pubs
-
-    def execute(self, userdata):
-        if self.parking_spot < 1 or self.parking_spot > 8:
-            return "drive_to_start"
-
-        pose = MoveBaseGoal()
-        pose.target_pose.header.frame_id = "map"
-        pose.target_pose.pose = WAYPOINT_MAP[str(self.parking_spot)]
-
-        self.client.send_goal(pose)
-        self.client.wait_for_result()
-
-        sound_msg = Sound()
-        sound_msg.value = Sound.ON
-        self.sound_pub.publish(sound_msg)
-        display_count(3, self.light_pubs, color_primary=Led.RED)
-        for _ in range(8):
-            self.rate.sleep()
-
-        rospy.sleep(2)
-
-        return "drive_to_start"
-
+        return "on_ramp"
+        
 
 class OnRamp(smach.State):
     def __init__(self, rate):
